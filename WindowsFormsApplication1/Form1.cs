8862a9c9fe6fcb40e59d2cd0451c59dc6e4cb899
Basel_Riaydh_Al_Thanon <baselzanon8@gmail.com>
2024-03-10 01:05:24 +0300

    Finished Calculator App

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int first_num,second_num;
+        int res = 0;
+        String op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "7";
+            else
+                textBox1.Text = "7";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "8";
+            else
+                textBox1.Text = "8";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "9";
+            else
+                textBox1.Text = "9";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "4";
+            else
+                textBox1.Text = "4";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "5";
+            else
+                textBox1.Text = "5";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "6";
+            else
+                textBox1.Text = "6";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "1";
+            else
+                textBox1.Text = "1";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "2";
+            else
+                textBox1.Text = "2";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "3";
+            else
+                textBox1.Text = "3";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                textBox1.Text = textBox1.Text + "0";
+            else
+                textBox1.Text = "0";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            second_num = Convert.ToInt32(textBox1.Text);
+            if (op == "+")
+            {
+                res = first_num + second_num;
+                textBox1.Text = res.ToString();
+            }
+
+            else if (op == "-")
+            {
+                res = first_num - second_num;
+                textBox1.Text = res.ToString();
+            }
+
+            else if (op == "*")
+            {
+                res = first_num * second_num;
+                textBox1.Text = res.ToString();
+            }
+            else if (op == "/")
+            {
+                res = first_num / second_num;
+                textBox1.Text = res.ToString();
+            }
+
+            else
+            {
+                res = first_num % second_num;
+                textBox1.Text = res.ToString();
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = textBox1.Text + "*";
+            op = "*";
+            textBox1.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = textBox1.Text + "/";
+            op = "/";
+            textBox1.Clear();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = textBox1.Text + "-";
+            op = "-";
+            textBox1.Clear();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = textBox1.Text + "+";
+            op = "+";
+            textBox1.Clear();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            first_num = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = textBox1.Text + "%";
+            op = "%";
+            textBox1.Clear();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        int res = 0;
+        int first_number, second_number;
+        String op;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                first_number = int.Parse(textBox1.Text);
+            }
+            catch(Exception)
+            { 
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                second_number = int.Parse(textBox2.Text);
+            }
+            catch (Exception)
+            {
+                textBox2.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            op = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (op == "*")
+            {
+                res = first_number * second_number;
+                textBox3.Text = res.ToString();
+            }
+
+            else if (op == "/")
+            {
+                res = first_number / second_number;
+                textBox3.Text = res.ToString();
+            }
+
+            else if (op == "%")
+            {
+                res = first_number % second_number;
+                textBox3.Text = res.ToString();
+            }
+
+            else if (op == "+")
+            {
+                res = first_number + second_number;
+                textBox3.Text = res.ToString();
+            }
+
+            else
+            {
+                res = first_number - second_number;
+                textBox3.Text = res.ToString();
+            }
+        }
+    }
+}
